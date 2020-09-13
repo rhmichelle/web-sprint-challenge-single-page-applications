488b115c7f99055888879a392a09a078c424c7cf
michelleRH <dev@michellerh.com>
2020-09-13 19:46:15 -0400

    git clone. Added Components

---
+++ src/Components/Pizza.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Pizza = () => {
+    return (
+        <div>
+            <h1>You have chosen to order Pizza...</h1>
+        </div>
+    )
+}
+
+export default Pizza;
